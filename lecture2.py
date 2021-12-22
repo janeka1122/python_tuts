9c211ffaaaec76fbb715ee799010f17a04e02fa3
Chinmay Harkawat <76124220+janeka1122@users.noreply.github.com>
2021-12-22 19:44:16 +0530

    Create lecture2.py

---
+++ lecture2.py
@@ -0,0 +1,51 @@
+# Multiline string 
+
+a = ''' This is a multiline string 
+        we notice that this extends to the 
+        next line '''
+
+# Note - String are arrays 
+
+a = "Hello World!"
+print(a[2])
+
+# Looping through a string 
+
+for ch in "Sheeba":
+  print(ch)
+
+# Printing length of the string 
+name = "Chinmay"
+print(len(name))
+
+# Searching in a sentence
+sentence = "This is a sample sentence"
+print("sample" in sentence)  # Returns Boolean Value
+
+if "is" in sentence:
+  print(" 'is' is a part of this sentence")
+  
+# Playing with strings
+
+intro = "My name is chinmay harkawat. I'm a second year mechanical engineering student"
+print(intro[0:7])  # prints "My name" only
+
+a = "Chinmay" 
+b = "Harkawat" 
+print(a+b) # concatincation of string 
+
+print("I am inserting \"this\" in double inverted commas" # Insert double quotation inside double quotation 
+
+# PYTHON LISTs
+      
+newlist = ["one", "two", "three"]
+print(newlist) # Output: ['one', 'two', 'three']
+      
+
+
+
+
+
+
+
+  
